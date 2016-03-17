c21bc70dd9f775d80e89b337820c9647713efaba
Kevin Harkins <kevinharkins@Kevins-MacBook-Pro.local>
2016-03-16 22:55:18 -0500

    initial commit, testing github pages

---
+++ r64.py
@@ -0,0 +1,82 @@
+
+weight = 1
+name = "Round of 64"
+
+winners = (
+    # South
+    None, #'Kansas', 'Austin Peay',
+    None, #'Colorado', 'Connecticut',
+    None, #'Maryland', 'SDSU',
+    None, #'California', 'Hawaii',
+    None, #'Arizona', 'Wichita St',
+    None, #'Miami', 'Buffalo',
+    None, #'Iowa', 'Temple',
+    None, #'Villanova', 'UNC Ashville',
+
+    # West
+    None, #'Oregon', 'Holy Cross/Southern',
+    None, #'St Joseph', 'Cincinnati',
+    None, #'Baylor', 'Yale',
+    None, #'Duke', 'UNCW',
+    None, #'Texas', 'UNI',
+    None, #'Texas A&M', 'Green Bay',
+    None, #'Oregon St', 'VCU',
+    None, #'Oklahoma', 'CSU Bakersfield',
+
+    # East
+    None, #'UNC', 'FGCU',
+    None, #'USC', 'Providence',
+    None, #'Indiana', 'Chattanooga',
+    None, #'Kentucky', 'Stony Brook',
+    None, #'Notre Dame', 'Mich/Tulsa',
+    None, #'West Virginia', 'SF Austin',
+    None, #'Wisconsin', 'Pittsburgh',
+    None, #'Xavier', 'Weber St',
+
+    # Midwest
+    None, #'Virginia', 'Hampton',
+    None, #'Texas Tech', 'Butler',
+    None, #'Purdue', 'Little Rock',
+    None, #'Iowa St', 'Iona',
+    None, #'Seton Hall', 'Gonzaga',
+    None, #'Utah', 'Fresno St',
+    None, #'Dayton', 'Syracuse',
+    None, #'Michigan St', 'MTSU',
+)
+
+
+picks= {
+
+'Justin':
+(
+    'Kansas', 'Connecticut', 'Maryland','Hawaii','Wichita St', 'Miami','Iowa','Villanova',
+    'Oregon','Cincinnati','Baylor','UNCW','UNI','Texas A&M','VCU','Oklahoma',
+    'UNC', 'Providence','Chattanooga','Kentucky','Notre Dame','West Virginia', 'Pittsburgh','Xavier',
+    'Virginia','Butler','Purdue', 'Iona','Seton Hall', 'Fresno St','Syracuse','Michigan St',
+),
+
+'Jerry':
+(
+     'Kansas', 'Connecticut', 'SDSU','California','Arizona', 'Miami','Iowa','Villanova',
+     'Oregon','Cincinnati','Baylor','Duke','Texas','Texas A&M','Oregon St','Oklahoma',
+     'UNC', 'USC','Indiana','Kentucky','Mich/Tulsa','West Virginia', 'Wisconsin','Xavier',
+     'Virginia','Butler','Purdue','Iowa St','Gonzaga', 'Utah','Syracuse','Michigan St',
+),
+
+'Jessica':
+(
+     'Kansas', 'Colorado', 'Maryland','California','Arizona', 'Miami','Temple','Villanova',
+     'Oregon','Cincinnati','Yale','Duke','Texas','Texas A&M','VCU','Oklahoma',
+     'UNC', 'USC','Indiana','Kentucky','Notre Dame','West Virginia', 'Wisconsin','Xavier',
+     'Virginia','Butler','Purdue','Iowa St','Gonzaga', 'Utah','Dayton','Michigan St',
+),
+
+'Owen':
+(
+     'Kansas', 'Colorado', 'Maryland','California','Arizona', 'Miami','Temple','Villanova',
+     'Oregon','Cincinnati','Baylor','Duke','Texas','Texas A&M','VCU','Oklahoma',
+     'UNC', 'USC','Chattanooga','Kentucky','Notre Dame','West Virginia', 'Pittsburgh','Xavier',
+     'Virginia','Butler','Purdue','Iowa St','Gonzaga', 'Utah','Syracuse','Michigan St',
+),
+
+}
---
+++ teams.py
@@ -0,0 +1,42 @@
+
+seeds = {
+# South
+'Kansas': 1, 'Austin Peay': 16, 
+'Colorado': 8, 'Connecticut': 9, 
+'Maryland': 5, 'SDSU': 12, 
+'California': 4, 'Hawaii': 13, 
+'Arizona': 6, 'Wichita St': 11, 
+'Miami': 3, 'Buffalo': 14, 
+'Iowa': 7, 'Temple': 10, 
+'Villanova': 2, 'UNC Ashville': 15, 
+
+# West
+'Oregon': 1, 'Holy Cross/Southern': 16, 
+'St Joseph': 8, 'Cincinnati': 9, 
+'Baylor': 5, 'Yale': 12, 
+'Duke': 4, 'UNCW': 13, 
+'Texas': 6, 'UNI': 11, 
+'Texas A&M': 3, 'Green Bay': 14, 
+'Oregon St': 7, 'VCU': 10, 
+'Oklahoma': 2, 'CSU Bakersfield': 15, 
+
+# East
+'UNC': 1, 'FGCU': 16, 
+'USC': 8, 'Providence': 9, 
+'Indiana': 5, 'Chattanooga': 12, 
+'Kentucky': 4, 'Stony Brook': 13, 
+'Notre Dame': 6, 'Mich/Tulsa': 11, 
+'West Virginia': 3, 'SF Austin': 14, 
+'Wisconsin': 7, 'Pittsburgh': 10, 
+'Xavier': 2, 'Weber St': 15, 
+
+# Midwest
+'Virginia': 1, 'Hampton': 16, 
+'Texas Tech': 8, 'Butler': 9, 
+'Purdue': 5, 'Little Rock': 12, 
+'Iowa St': 4, 'Iona': 13, 
+'Seton Hall': 6, 'Gonzaga': 11, 
+'Utah': 3, 'Fresno St': 14, 
+'Dayton': 7, 'Syracuse': 10, 
+'Michigan St': 2, 'MTSU': 15, 
+}
