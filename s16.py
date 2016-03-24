ef76d5c41b96b08e06b5c3b36b2a09155d6f1190
Kevin Harkins <kevinharkins@kevins-imac.dhcp.mc.vanderbilt.edu>
2016-03-24 15:55:39 -0500

    waiting on Herrison & Suzie

---
+++ s16.py
@@ -0,0 +1,85 @@
+
+weight = 6
+name = "Round of 32"
+
+winners = (
+    # South
+    'Kansas','Maryland',
+    'Miami','Villanova',
+
+    # West
+    'Oregon','Duke',
+    'Texas A&M','Oklahoma',
+
+    # East
+    'UNC','Indiana',
+    'Notre Dame','Wisconsin',
+
+    # Midwest
+    'Virginia','Iowa St',
+    'Gonzaga','Syracuse',
+)
+
+
+picks= {
+
+'Justin':
+(
+    'Kansas','Villanova',
+    'Duke','Oklahoma',
+    'Indiana','Notre Dame',
+    'Virginia','Syracuse',
+),
+
+'Jerry':
+(
+    'Kansas','Villanova',
+    'Oregon','Oklahoma',
+    'UNC','Wisconsin',
+    'Virginia','Gonzaga',
+),
+
+'Jessica':
+(
+    'Kansas','Miami',
+    'Oregon','Oklahoma',
+    'Indiana','Wisconsin',
+    'Virginia','Gonzaga',
+),
+
+'Owen':
+(
+    'Kansas','Villanova',
+    'Oregon','Oklahoma',
+    'Indiana','Wisconsin',
+    'Virginia','Gonzaga',
+),
+
+'Suzie':
+(
+
+),
+
+
+'Chris':
+(
+    'Kansas','Villanova',
+    'Duke','Oklahoma',
+    'UNC','Wisconsin',
+    'Virginia','Syracuse',
+),
+
+'Kevin':
+(
+    'Kansas','Villanova',
+    'Duke','Texas A&M',
+    'UNC','Notre Dame',
+    'Virginia','Gonzaga',
+),
+
+'Herrison':
+(
+
+),
+
+}
