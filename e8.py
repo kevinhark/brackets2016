5fc2cfd52414f4b02814818587c4bfb0ad52c68e
Kevin Harkins <kevinharkins@Kevins-MacBook-Pro.local>
2016-03-26 15:47:51 -0500

    picks for the start of elite 8

---
+++ e8.py
@@ -0,0 +1,63 @@
+
+weight = 40
+name = "Elite 8"
+
+winners = (
+    # South
+    None, #'Kansas','Villanova',
+
+    # West
+    None, #'Oregon','Oklahoma',
+
+    # East
+    None, #'UNC','Notre Dame',
+
+    # Midwest
+    None, #'Virginia','Syracuse',
+)
+
+
+picks= {
+
+'Justin':
+(
+    'Kansas','Oklahoma','UNC','Virginia',
+),
+
+'Jerry':
+(
+     'Kansas','Oregon','UNC','Virginia',
+),
+
+'Jessica':
+(
+    'Kansas','Oklahoma','UNC','Virginia',
+),
+
+'Owen':
+(
+     'Kansas','Oregon','UNC','Virginia',
+),
+
+'Suzie':
+(
+    'Kansas','Oklahoma','UNC','Virginia',
+),
+
+
+'Chris':
+(
+    'Villanova','Oklahoma','UNC','Syracuse',
+),
+
+'Kevin':
+(
+    'Kansas','Oregon','UNC','Virginia',
+),
+
+'Herrison':
+(
+    'n/a','n/a','n/a','n/a',
+),
+
+}
