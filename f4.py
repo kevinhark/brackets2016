8793c5eceff493dd7ae9ed2fe432cb13df1fac19
Kevin Harkins <kevinharkins@Kevins-MacBook-Pro.local>
2016-04-02 14:03:43 -0500

    final four picks

---
+++ f4.py
@@ -0,0 +1,54 @@
+
+weight = 80
+name = "Final 4"
+
+winners = (
+           None, #'Villanova','Oklahoma',
+           None, #'UNC','Syracuse',
+           )
+
+
+picks= {
+
+'Justin':
+(
+    'Villanova','UNC',
+),
+
+'Jerry':
+(
+    'Oklahoma','UNC'
+),
+
+'Jessica':
+(
+    'Oklahoma','UNC',
+),
+
+'Owen':
+(
+    'Oklahoma','Syracuse',
+),
+
+'Suzie':
+(
+    'Oklahoma','Syracuse',
+),
+
+
+'Chris':
+(
+    'Villanova','UNC',
+),
+
+'Kevin':
+(
+    'Oklahoma','UNC'
+),
+
+'Herrison':
+(
+     'n/a','n/a',
+),
+
+}
