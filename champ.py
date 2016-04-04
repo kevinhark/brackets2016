0f2cdeccba08860cf9a21781566d098542395234
Kevin Harkins <kevinharkins@Kevins-MacBook-Pro.local>
2016-04-04 17:54:38 -0500

    championship picks

---
+++ champ.py
@@ -0,0 +1,53 @@
+
+weight = 180
+name = "Championship Game"
+
+winners = (
+           None, #'Villanova','UNC',
+           )
+
+
+picks= {
+
+'Justin':
+(
+    'Villanova',
+),
+
+'Jerry':
+(
+     'UNC',
+),
+
+'Jessica':
+(
+     'Villanova',
+),
+
+'Owen':
+(
+     'Villanova',
+),
+
+'Suzie':
+(
+     'UNC',
+),
+
+
+'Chris':
+(
+    'UNC',
+),
+
+'Kevin':
+(
+    'UNC',
+),
+
+'Herrison':
+(
+     'n/a',
+),
+
+}
